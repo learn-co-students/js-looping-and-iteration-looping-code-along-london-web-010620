3befb6c6fd773c01d6b7a5e3068bf00ba08a40fd
Harriet Bicknell <harrietindubai@gmail.com>
2020-02-17 14:48:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+const newArray = []
+
+function writeCards(names, birthday) {
+    for(let i = 0; i < names.length; i++) {
+        newArray.push(`Thank you, ${names[i]}, for the wonderful ${birthday} gift!`);
+    };
+    return newArray;
+}
+
+function countDown(number) {
+    let i = -1;
+    while (i < number) {
+        console.log(number--);
+    }
+};
